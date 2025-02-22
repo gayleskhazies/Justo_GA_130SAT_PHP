0cc9e68b2339ec7e20e6e7a647184586246f50a5
Gayle Justo <gayle.justo25@gmail.com>
2025-02-22 22:41:08 +0800

    0222_Vid16_Forms-and-CSRF-Explained-with-Examples

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,38 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Job {
-    public static function all(): array
+class Job extends Model {
+    use HasFactory;
+
+    protected $table = 'job_listings';
+
+    protected $guarded = [];
+
+    public function employer()
     {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40,000'
-            ]
-        ];
-    }
-
-    public static function find(int $id): array
-    {
-        $job = Arr::first(static::all(), fn($job) => $job['id'] == $id);
-
-        if (! $job) {
-            abort(404);
-        }
-
-        return $job;
+        return $this->belongsTo(Employer::class);
     }
 }
